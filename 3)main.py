e08d2f2a98d610405fc7e5d5193a674b97afc787
hc371192-boop <hc371192@gmail.com>
2025-08-16 18:10:43 +0530

    Add files via upload

---
+++ 3)main.py
@@ -0,0 +1,13 @@
+# QUE-1
+name = input("Enter your name: ")
+print("Good morning, " + name)
+# QUE-2
+letter = '''Dear <|Name|>,
+You are selected on <|Date|>
+for the job role of Software Engineer.'''
+print(letter.replace("<|Name|>", "Yudhishtar Chaudhary").replace("<|Date|>", "28 September 2025"))
+# QUE-3
+name = "Yudhishtar Chaudhary"
+print(name.find(" "))
+letter = "Dear Harsh, \nthis python course is nice. \nThanks!"
+print(letter)
