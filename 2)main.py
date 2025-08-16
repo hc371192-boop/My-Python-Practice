c2fa3cf3b4ce2a73a32a25016b9ba170d4e2b12f
hc371192-boop <hc371192@gmail.com>
2025-08-16 18:09:32 +0530

    Add files via upload

---
+++ 2)main.py
@@ -0,0 +1,25 @@
+a = 23
+print(type(a))
+b = 3.14
+print(type(b))
+c = "Hello, World!"
+print(type(c))
+d = input("Enter the word you want to print: ")
+print(d)
+e = int(input("Enter first number: "))
+f = int(input("Enter second number: "))
+print("Sum is: ", e + f)
+h = 9999999
+i = 3
+print("The remainder of the equation when h is divided by i is", h % i)
+j = input("Enter the value: ")
+print(type(j))
+k = 34
+l = 80
+print("k is smaller than l which is", k < l)
+m = int(input("Enter a number: "))
+n = int(input("Enter a number: "))
+print("The average of the two numbers is", (m + n) / 2)
+o = int(input("Enter a number: "))
+print("The square of the number is", o ** 2)
+
