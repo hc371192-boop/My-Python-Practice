a6f4c1a5a9db19a578a30c18e1389c1f12496ad7
hc371192-boop <hc371192@gmail.com>
2025-08-16 18:01:49 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-# QUE-1
-name = input("Enter your name: ")
-print("Good morning, " + name)
-# QUE-2
-letter = '''Dear <|Name|>,
-You are selected on <|Date|>
-for the job role of Software Engineer.'''
-print(letter.replace("<|Name|>", "Yudhishtar Chaudhary").replace("<|Date|>", "28 September 2025"))
-# QUE-3
-name = "Yudhishtar Chaudhary"
-print(name.find(" "))
-letter = "Dear Harsh, \nthis python course is nice. \nThanks!"
-print(letter)
+a = 23
+print(type(a))
+b = 3.14
+print(type(b))
+c = "Hello, World!"
+print(type(c))
+d = input("Enter the word you want to print: ")
+print(d)
+e = int(input("Enter first number: "))
+f = int(input("Enter second number: "))
+print("Sum is: ", e + f)
+h = 9999999
+i = 3
+print("The remainder of the equation when h is divided by i is", h % i)
+j = input("Enter the value: ")
+print(type(j))
+k = 34
+l = 80
+print("k is smaller than l which is", k < l)
+m = int(input("Enter a number: "))
+n = int(input("Enter a number: "))
+print("The average of the two numbers is", (m + n) / 2)
+o = int(input("Enter a number: "))
+print("The square of the number is", o ** 2)
+
