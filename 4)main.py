e2c4af3d14e6756feb856f86b594fde370e8cc4a
Yudhishtar_Chaudhary_JAAT💀💀 <hc371192@gmail.com>
2025-08-17 15:16:30 +0530

    Add files via upload

---
+++ 4)main.py
@@ -0,0 +1,35 @@
+#QUE-1
+# fruits = []
+# f1 = input("Enter the first fruit: ")
+# fruits.append(f1)
+# f2 = input("Enter the first fruit: ")
+# fruits.append(f2)
+# f3 = input("Enter the first fruit: ")
+# fruits.append(f3)
+# f4 = input("Enter the first fruit: ")
+# fruits.append(f4)
+# f5 = input("Enter the first fruit: ")
+# fruits.append(f5)
+# print(fruits)
+#QUE-2
+# marks = []
+# s1 = int(input("Enter the marks: "))
+# marks.append(s1)
+# s2 = int(input("Enter the marks: "))
+# marks.append(s2)
+# s3 = int(input("Enter the marks: "))
+# marks.append(s3)
+# s4 = int(input("Enter the marks: "))
+# marks.append(s4)
+# s5 = int(input("Enter the marks: "))
+# marks.append(s5)
+# s6 = int(input("Enter the marks: "))
+# marks.append(s6)
+# marks.sort()
+# print(marks)
+#QUE-3
+# T = [1,3,5,7,9]
+# print(sum(T))
+#QUE-4
+# a = (7, 0, 8, 0, 0, 9)
+# print(a.count(0))
